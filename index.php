c4afea0144e4d64bc224b26143eacc256e3e1885
Eduardo Araújo <110600044+araujoedu7@users.noreply.github.com>
2023-10-20 10:05:43 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</head>
+
+<body style="background-color: #F2F2F2;">
+    <div class="conteiner">
+        <div class="row">
+            <div class="col-lg-4 offset-lg-4">
+                <div class="card">
+                    <div class="card-body">
+                        <form action="login.php" method="POST">
+                            <div>
+                                <div class="mb-3">
+                                    <label>Usuários</label>
+                                    <input type="text" name="usuario" class="form-control">
+                                </div>
+                            </div>
+                            <div>
+                                <div class="mb-3">
+                                    <label>Senha</label>
+                                    <input type="password" name="senha" class="form-control">
+                                </div>
+                            </div>
+                            <div>
+                                <div class="mb-3">
+
+                                    <button type="submit" class="btn btn-primary">Logar</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
