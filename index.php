b8fcebb5afb6c6848b3a2ae8d55f1527ebbc5862
araujoedu7 <ea144027@gmail.com>
2023-10-23 16:23:16 -0300

    bapo

--- index.php
+++ index.php
@@ -7,34 +7,41 @@
     <title>Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css">
 </head>
 
-<body style="background-color: #F2F2F2;">
-    <div class="conteiner">
-        <div class="row">
-            <div class="col-lg-4 offset-lg-4">
-                <div class="card">
-                    <div class="card-body">
-                        <form action="login.php" method="POST">
-                            <div>
-                                <div class="mb-3">
-                                    <label>Usuários</label>
-                                    <input type="text" name="usuario" class="form-control">
-                                </div>
-                            </div>
-                            <div>
-                                <div class="mb-3">
-                                    <label>Senha</label>
-                                    <input type="password" name="senha" class="form-control">
-                                </div>
-                            </div>
-                            <div>
-                                <div class="mb-3">
+<body>
 
-                                    <button type="submit" class="btn btn-primary">Logar</button>
+    <div class="login">
+        <div class="conteiner">
+            <div class="row">
+                <div class="col-lg-4 offset-lg-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h3>Acesso Restrito</h3>
+                        </div>
+                        <div class="card-body">
+                            <form action="login.php" method="POST">
+                                <div>
+                                    <div class="mb-3">
+                                        <label>Usuários</label>
+                                        <input type="text" name="usuario" class="form-control">
+                                    </div>
                                 </div>
-                            </div>
-                        </form>
+                                <div>
+                                    <div class="mb-3">
+                                        <label>Senha</label>
+                                        <input type="password" name="senha" class="form-control">
+                                    </div>
+                                </div>
+                                <div>
+                                    <div class="mb-3">
+
+                                        <button type="submit" class="btn btn-primary">Logar</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
