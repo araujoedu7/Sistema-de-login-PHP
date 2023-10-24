816cf69209933796c3e538f78d3a96b85c7d1860
antonysantos71 <ribeiroantony02.56@gmail.com>
2023-10-24 10:05:42 -0300

    adição da dashboard e da verificação do login

---
+++ dashboard.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+if (empty($_SESSION)) {
+    print "<script>location.href='index.php'</script>";
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-light bg-ligth">
+        <div class="conteiner-fluid">
+            <a class="navbar-brand">Seja bem vindo!</a>
+
+            <?php
+            print "Olá," . $_SESSION["nome"];
+            print "<a href='logout.php' class='btn btn-danger'>Sair</a> "
+            ?>
+        </div>
+    </nav>
+
+    
+    <h2>Coloque seu conteúdo aqui</h2>
+</body>
+
+</html>
--- login.php
+++ login.php
@@ -1,1 +1,34 @@
-php
+<?php
+include('config.php');
+
+session_start();
+if (empty($_POST) or (empty($_POST["usuario"]) or (empty($_POST["senha"])))) {
+    print "<script>location.href='index.php'</script>";
+};
+
+
+
+$usuario = $_POST["usuario"];
+$senha = $_POST["senha"];
+
+
+$sql = "SELECT * FROM usuarios
+ WHERE  usuarios = '{$usuarios}', 
+ 
+ AND senha = '{$senha}'";
+
+$res = $conn->query($sql) or die($sconn->erro);
+
+$row = $res->fetch_object();
+
+$qtd = $res->num_rows;
+
+if ($qtd > 0) {
+    $_SESSION["usuarios"] = $usuario;
+    $_SESSION["nome"] = $row->nome;
+    $_SESSION["tipo"] = $row->tipo;
+    print "<script>location.href='dashboard.php'</script>";
+} else {
+    print "<script>alert('Usuário e/ou senha incorretos');</script>";
+    print "<script>location.href='dashboard.php'</script>";
+}
